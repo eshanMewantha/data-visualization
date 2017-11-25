7b4caa9eb68ecf96033cc85f4ed45e6bcff2d926
Eshan Mewantha Herath <eshanMewantha@users.noreply.github.com>
2017-11-25 21:08:18 +0530

    adding bar chart visualization

---
+++ python/bar_chart_titanic.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Read data to Dataframe (891 rows)
+df = pd.read_csv('../data/titanic/titanic_data.csv')
+
+# See column names
+print(df.columns.values)
+
+# Calculate the number of survivors by passenger class
+total_survivors_by_class = df.groupby('Pclass')['Survived'].sum()
+print(total_survivors_by_class)
+
+# Calculate the number of survivors by passenger sex
+total_survivors_by_sex = df.groupby('Sex')['Survived'].sum()
+print(total_survivors_by_sex)
+
+# Visualize the Survival and Non-survival of passengers by class and by gender
+_, (axis1, axis2) = plt.subplots(1, 2, figsize=(12, 4))
+
+ax = total_survivors_by_class.plot.bar(ax=axis1, color='#5975A4', title='Total Survivors by Class', sharey=True)
+ax.set_ylabel('Total Survivors')
+ax.set_ylim(0.0, max(total_survivors_by_class) + 10)
+ax = total_survivors_by_sex.plot.bar(ax=axis2, color='#5F9E6E', title='Total Survivors by Sex', sharey=True)
+ax.set_ylim(0.0, max(total_survivors_by_sex) + 10)
+
+plt.show()
+
